e4190b92e41cbf7081a7f753572606eedfb46ae5
n0k1b <nokibevon7@gmail.com>
2021-11-22 11:51:31 +0600

    domestic profit added

--- app/Http/Controllers/RechargeController.php
+++ app/Http/Controllers/RechargeController.php
@@ -833,11 +833,14 @@ class RechargeController extends Controller
             if($xml2->RESULT == 0){
 
                 if(a::user()->role != 'admin'){
-                    $reseller_commission = ($sku_amount['1']/100)*a::user()->reseller_profit->domestic_recharge_profit;
-                    $admin_commission = ($sku_amount['1']/100)*a::user()->admin_recharge_commission;
+                   // $reseller_commission = ($sku_amount['1']/100)*a::user()->reseller_profit->domestic_recharge_profit;
+                   // $admin_commission = ($sku_amount['1']/100)*a::user()->admin_recharge_commission;
                     $cost = $sku_amount['1'];
 
                     $admin_given_profit = ($prof->commission/100)*a::user()->admin_recharge_commission;
+                    $reseller_commission = $admin_given_profit;
+                    $admin_commission = $prof->commission - $reseller_commission;
+                    //$admin_commission = $admin_given_profit
 
                     $minus = a::user()->update([
                         'wallet' => a::user()->wallet - $cost + $admin_given_profit,
