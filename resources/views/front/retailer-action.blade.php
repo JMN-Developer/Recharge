99e12d45c5fc525381abc1b1c12fce13fbff374d
n0k1b <nokibevon7@gmail.com>
2021-11-29 11:20:33 +0600

    retailer action pin and css issue solved

--- resources/views/front/retailer-action.blade.php
+++ resources/views/front/retailer-action.blade.php
@@ -14,7 +14,7 @@
 
   <link rel="stylesheet" href="{{asset('css/style.css')}}">
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" ></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css"  />
+
     <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
     <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
 <link rel="icon" href="https://jmnation.com/images/jm-transparent-logo.png"></head>
@@ -203,7 +203,7 @@
           $.ajax({
               type: "GET",
               dataType: "json",
-              url: '/changePin',
+              url: '/retailer/changePin',
               data: {'status': status, 'user_id': user_id},
               success: function(data){
                 console.log(data.success)
@@ -215,6 +215,13 @@
 @endsection
 
 @section('scripts')
+<!-- jQuery -->
 
-
+<!-- Bootstrap -->
+<script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+<!-- Theme JS -->
+<script src="{{asset('js/admin.js')}}"></script>
+<script src="{{asset('plugin/intl-tel-input/js/intlTelInput.js')}}"></script>
+<!-- Custom JS -->
+<script src="{{asset('js/custom.js')}}"></script>
 @endsection
