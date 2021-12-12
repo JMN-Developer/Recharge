ed7a68a9b1021751cf5e5e6adb56fcf14a776012
n0k1b <nokibevon7@gmail.com>
2021-12-12 15:46:36 +0600

    wallet request updated

--- resources/views/front/wallet-request.blade.php
+++ resources/views/front/wallet-request.blade.php
@@ -4,10 +4,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}" />
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
   <title>Wallet Request</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{asset('css/fontawesome-free/css/all.min.css')}}">
   <!-- Theme style -->
