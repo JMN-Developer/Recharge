276a6636e6746b7f5c4f517ef81474ec0d5f3a9d
n0k1b <nokibevon7@gmail.com>
2022-01-19 16:03:20 +0600

    new transaction api addedd

--- app/Http/Controllers/DtOneController.php
+++ app/Http/Controllers/DtOneController.php
@@ -146,114 +146,21 @@ class DtOneController extends Controller
         $skuId = $request->id;
         $transaction =  new GenerateTransactionId(a::user()->id,12);
         $txid = $transaction->transaction_id();
-       $data = $this->dtone->recharge($skuId,$txid,$number);
-    //     $tmp_data = '{
-    //         "benefits":[
-    //            {
-    //               "additional_information":null,
-    //               "amount":{
-    //                  "base":100,
-    //                  "promotion_bonus":0,
-    //                  "total_excluding_tax":100
-    //               },
-    //               "type":"CREDITS",
-    //               "unit":"BDT",
-    //               "unit_type":"CURRENCY"
-    //            }
-    //         ],
-    //         "confirmation_expiration_date":"2021-12-23T15:58:53.049511000Z",
-    //         "creation_date":"2021-12-23T14:58:53.049511000Z",
-    //         "credit_party_identifier":{
-    //            "mobile_number":"+8801845318609"
-    //         },
-    //         "external_id":"23122021145852002312",
-    //         "id":2237551912,
-    //         "prices":{
-    //            "retail":{
-    //               "amount":1.5,
-    //               "fee":0,
-    //               "unit":"EUR",
-    //               "unit_type":"CURRENCY"
-    //            },
-    //            "wholesale":{
-    //               "amount":1.2,
-    //               "fee":0,
-    //               "unit":"EUR",
-    //               "unit_type":"CURRENCY"
-    //            }
-    //         },
-    //         "product":{
-    //            "description":"",
-    //            "id":3425,
-    //            "name":"100 BDT",
-    //            "operator":{
-    //               "country":{
-    //                  "iso_code":"BGD",
-    //                  "name":"Bangladesh",
-    //                  "regions":null
-    //               },
-    //               "id":1446,
-    //               "name":"Robi Bangladesh",
-    //               "regions":null
-    //            },
-    //            "regions":null,
-    //            "service":{
-    //               "id":1,
-    //               "name":"Mobile"
-    //            },
-    //            "tags":[
-    //               "AIRTIME"
-    //            ],
-    //            "type":"FIXED_VALUE_RECHARGE"
-    //         },
-    //         "promotions":null,
-    //         "rates":{
-    //            "base":83.3333333333333,
-    //            "retail":66.6666666666667,
-    //            "wholesale":83.3333333333333
-    //         },
-    //         "status":{
-    //            "class":{
-    //               "id":1,
-    //               "message":"CREATED"
-    //            },
-    //            "id":10000,
-    //            "message":"CREATED"
-    //         }
-    //      }';
-
-    // $tmp_data = json_decode($tmp_data);
-     //$data = ['status'=>true,'payload'=>$tmp_data];
+        $data = $this->dtone->recharge($skuId,$txid,$number);
+     
     //  //file_put_contents('test.txt',$tmp_data->responseCode);
 
         if($data['status']){
 
-            if($data['payload']->status->class->message=='REJECTED')
-            {
-                return ['status'=>false,'message'=>$data['payload']->status->message];
-            }
-
-            if($data['payload']->status->class->message=='CANCELLED')
-            {
-                return ['status'=>false,'message'=>$data['payload']->status->message];
-            }
-
-            if($data['payload']->status->class->message=='DECLINED')
-            {
-                return ['status'=>false,'message'=>$data['payload']->status->message];
-            }
-
-
-           // file_put_contents('test.txt',$data['payload']);
-        $recharge = $this->create_recharge($data['payload'],$number,$txid,$country_code,$request->service_charge);
+          $recharge = $this->create_recharge($data['payload'],$number,$txid,$country_code,$request->service_charge);
          UpdateWallet::update($recharge);
           $this->update_balance($data['payload']->prices->retail->amount,$data['payload']->prices->wholesale->amount);
         return ['status'=>true,'message'=>'Recharge Successfull'];
         }
     else
     {
-        $data = $data['payload'];
-        return ['status'=>false,'message'=>$data->message];
+      
+        return ['status'=>false,'message'=>$data['message']];
     }
 
 
--- app/Services/DtOneProvider.php
+++ app/Services/DtOneProvider.php
@@ -66,41 +66,37 @@ class DtOneProvider
 
     }
 
-    public function confirmation($txid)
+    public function transaction($transaction_id)
+    
     {
         $client = new \GuzzleHttp\Client(['http_errors' => false]);
-        $response = $client->post('https://dvs-api.dtone.com/v1/async/transactions/'.$txid.'/confirm',[
-            'headers' => [
-            'Authorization'=>'Basic '.$this->access_token,
-            'Content-Type' => 'application/json'
+        $operator_request = $client->get('https://dvs-api.dtone.com/v1/transactions/'.$transaction_id,['headers' => [
+            'Authorization'     => 'Basic '.$this->access_token,
+            'Accept'=> 'application/json',
 
-            ],
-            'verify' => false,
-            'json' => [
-                    'transaction_id' => $txid,
+            ],'verify' => false]);
 
+            $status = $operator_request->getStatusCode();
 
-                    ]
-        ]);
+            $operator_response = $operator_request->getBody();
+            // file_put_contents('test.txt',$operator_response);
 
-
-
-
-            $status = $response->getStatusCode();
-            $operator_response = $response->getBody();
-           
             $operator_response = json_decode($operator_response);
-            if($status == 202)
+           
+            if($status == 200)
             {
-                return ['status'=>true,'message'=>$operator_response->status->message];
+
+            return ['payload'=>$operator_response,'status'=>true];
             }
             else
             {
-                return ['status'=>false,'message'=>$operator_response];
+
+            return ['payload'=>$operator_response,'status'=>false];
             }
 
-
     }
+
+    
 
 
     public function recharge($sku_id,$txid,$mobile)
@@ -117,33 +113,103 @@ class DtOneProvider
             'json' => [
                     'product_id' => $sku_id,
                     'external_id'=>$txid,
-                    'credit_party_identifier'=>['mobile_number'=>$mobile]
+                    'credit_party_identifier'=>['mobile_number'=>$mobile],
+                    'auto_confirm'=>true
 
                     ]
         ]);
         $status = $response->getStatusCode();
-
-
+       
         $response = json_decode($response->getBody());
 
 
         if($status == '200' || $status == '201')
             {
+                if($response->status->message =='CONFIRMED')
+                {
+                    sleep(3);
+                    $transaction_report = $this->transaction($response->id);
+                    if($transaction_report['status'] == true)
+                    {
+                        if($transaction_report['payload']->status->message == 'COMPLETED' || $transaction_report['payload']->status->message == 'SUBMITTED' )
+                        {
+                           
+                        return ['payload'=>$transaction_report['payload'],'status'=>true];
+                        }
+                        else
+                        {
+                        
+                        return ['message'=>$transaction_report['payload']->status->message,'status'=>false];
+                        }
+                    }
+                    else
+                    {
+                        return ['message'=>$transaction_report['payload']->status->message,'status'=>false];
+                    }
 
-            $confirmation = $this->confirmation($response->id);
-            if($confirmation['status']==true)
-            {
-            return ['payload'=>$response,'status'=>true];
-            }
-            else{
-                return ['payload'=>$confirmation['message'],'status'=>false];
-            }
+                }
+
+                // file_put_contents('test.txt',json_encode($response));
+        //     $confirmation = $this->confirmation($response->id);
+            
+        // //   file_put_contents('test.txt',json_encode($response));
+        //     if($confirmation['status']==true)
+        //     {
+        //        $transaction_report = $this->transaction($response->id); 
+        //        if($transaction_report['status']==true)
+        //       {  
+        //         return ['payload'=>$transaction_report[],'status'=>false];
+        //       }
+        //       else
+        //       {
+        //         return ['payload'=>$response,'status'=>false];
+        //       }
+          
+        //     }
+        //     else{
+        //         return ['payload'=>$confirmation['message'],'status'=>false];
+        //     }
             }
             else
             {
 
-            return ['payload'=>$response,'status'=>false];
+            return ['message'=>'Some Error Occured. Please try again after sometimes','status'=>false];
             }
+    }
+
+
+    public function confirmation($txid)
+    {
+        $client = new \GuzzleHttp\Client(['http_errors' => false]);
+        $response = $client->post('https://dvs-api.dtone.com/v1/async/transactions/'.$txid.'/confirm',[
+            'headers' => [
+            'Authorization'=>'Basic '.$this->access_token,
+            'Content-Type' => 'application/json'
+
+            ],
+            'verify' => false,
+            'json' => [
+                    'transaction_id' => $txid,
+                    ]
+        ]);
+
+
+
+
+            $status = $response->getStatusCode();
+            $operator_response = $response->getBody();
+         
+            $operator_response = json_decode($operator_response);
+            if($status == 202)
+            {
+                return ['status'=>true,'message'=>$operator_response->status->message];
+            }
+            else
+            {
+                return ['status'=>false,'message'=>$operator_response];
+            }
+
+
     }
 
 }
