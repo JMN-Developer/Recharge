ab17fc3f1e38ebede3ba4af873c2751919acaa8a
n0k1b <nokibevon7@gmail.com>
2022-02-06 10:58:13 +0600

    flight router error solve initiate

--- routes/web.php
+++ routes/web.php
@@ -354,10 +354,17 @@ Route::group(['prefix' => 'phone','middleware'=>['auth']], function()
 
 });
 
+Route::get('{any}', function () {
+    return view('welcome');
+})->where('any', '.*');
+
 Route::group(['prefix' => 'flights'], function()
 {
+    Route::get('{any}', function () {
+        return view('front.add-flight');
+    })->where('any', '.*');
 
-    Route::get('/', [FlightController::class,'add_flight'])->name('add-flight');
+  // Route::get('/', [FlightController::class,'add_flight'])->name('add-flight');
 
 
 });
